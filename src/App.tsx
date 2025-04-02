45ed3941fc2a6b8ff2bd68cf645246075cdfb896
rockethelll <rockethell@hotmail.com>
2025-04-02 16:34:04 +0200

    replace axios with @tanstack/react-query and add related devtools; update App component to use custom fetch hook and display loading/error states;

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
+import { useFetch, Property } from "./hooks/useFetch";
+
 function App() {
+  const { data, isLoading, error } = useFetch();
   return (
     <div className='bg-red-500'>
-      <h1>Hello World</h1>
+      {isLoading && <div>Loading...</div>}
+      {error && <div>Error: {error.message}</div>}
+      {data && (
+        <div>
+          {data.map((property: Property) => (
+            <div key={property.id}>{property.title}</div>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
---
+++ src/hooks/useFetch.ts
@@ -0,0 +1,40 @@
+import { useQuery } from '@tanstack/react-query';
+import mockData from '../data/mockData.json';
+
+const REAL_API_URL =
+  'https://raw.githubusercontent.com/devchallenges-io/curriculum/refs/heads/main/4-frontend-libaries/challenges/group_1/data/property-listing-data.json';
+const USE_MOCK = import.meta.env.REACT_APP_USE_MOCK === 'true';
+
+export type Property = {
+  id: number;
+  title: string;
+  description: string;
+  price: number;
+  rating: number;
+  superhost: boolean;
+  location: string;
+  capacity: {
+    people: number;
+    bedroom: number;
+  };
+  image: string;
+}
+
+export const useFetch = () => {
+  const { data, isLoading, error } = useQuery({
+    queryKey: ['properties'],
+    queryFn: async () => {
+      if (USE_MOCK) return mockData;
+
+      const response = await fetch(REAL_API_URL);
+
+      if (!response.ok) throw new Error('Failed to fetch data');
+
+      return response.json();
+    },
+    staleTime: Infinity, // Never re-fetch data
+    gcTime: Infinity, // Changed from cacheTime to gcTime
+  });
+
+  return { data, isLoading, error };
+};
